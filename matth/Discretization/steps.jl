0971a3bdca56b5a2e1edb0918780136857469809
claeys01 <matthias.claeys2002@gmail.com>
2025-09-29 18:54:11 +0200

    small edits

--- matth/Discretization/steps.jl
+++ matth/Discretization/steps.jl
@@ -10,6 +10,8 @@ end
 
 
 function custom_mom_step!(flow::WaterLily.Flow{N}, pois::WaterLily.AbstractPoisson; λ=WaterLily.quick, udf=nothing, kwargs...) where N
+    """"i want to get the RHS of the momentum equation, should be conv_diff - grad_p"""
+
     flow.u⁰ .= flow.u; WaterLily.scale_u!(flow,0); t₁ = sum(flow.Δt); t₀ = t₁-flow.Δt[end]
 
     WaterLily.conv_diff!(flow.f,flow.u⁰,flow.σ,λ;ν=flow.ν,perdir=flow.perdir)
--- matth/bottlenecks/quantify.jl
+++ matth/bottlenecks/quantify.jl
@@ -10,10 +10,10 @@ includet("../vortex_shedding.jl")
 
 
 sim_shedding = circle_shedding(mem=Array)
-t_end = 50.0
+t_end = 30.0
 
 @timeit to "sim_step!" begin
-    gif = sim_gif!(sim_shedding;duration=t_end,clims=(-5,5),plotbody=true, timer=to)
+    # gif = sim_gif!(sim_shedding;duration=t_end,clims=(-5,5),plotbody=true, timer=to)
     # sim_step!(sim_shedding, t_end; verbose=true, timer=to)
 end
 
@@ -29,6 +29,22 @@ end
 # plt = flood(ω, clims=(-5,5), border=:none)
 # display(plt)
 
-show(to)
-display(gif)
+function get_forces!(sim,t)
+    sim_step!(sim,t,remeasure=false; verbose=true)
+    force = WaterLily.pressure_force(sim)
+    force./(0.5sim.L*sim.U^2) # scale the forces!
+end
+
+# Simulate through the time range and get forces
+time = 1:0.1:100 # time scale is sim.L/sim.U
+forces = [get_forces!(sim_shedding,t) for t in time];
+
+#Plot it
+plot(time,[first.(forces) last.(forces)],
+    labels=["drag" "lift"],
+    xlabel="tU/L",
+    ylabel="Pressure force coefficients")
+
+# show(to)
+# display(gif)
 # reset_timer!(to)
