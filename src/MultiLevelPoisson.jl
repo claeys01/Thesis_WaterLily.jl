76db3c34b5aee5974dd0eb189e0ce98fbf5e9cda
claeys01 <matthias.claeys2002@gmail.com>
2025-09-29 18:53:47 +0200

    edditted function such that it will work without timer being passed to it

--- src/MultiLevelPoisson.jl
+++ src/MultiLevelPoisson.jl
@@ -85,21 +85,18 @@ mult!(ml::MultiLevelPoisson,x) = mult!(ml.levels[1],x)
 residual!(ml::MultiLevelPoisson,x) = residual!(ml.levels[1],x)
 
 function solver!(ml::MultiLevelPoisson;tol=1e-4,itmx=32,kwargs...)
-    to = get(kwargs, :timer, nothing)
     p = ml.levels[1]
-    @timeit to "residual" residual!(p)
-    @timeit to "L₂" r₂ = L₂(p)
+    residual!(p)
+    r₂ = L₂(p)
     nᵖ=0; @log ", $nᵖ, $(L∞(p)), $r₂\n"
-    @timeit to "Vcycle loop" begin
-        while nᵖ<itmx
-            @timeit to "Vcycle" Vcycle!(ml)
-            @timeit to "smooth" smooth!(p)
-            @timeit to "L₂" r₂ = L₂(p)
-            nᵖ+=1
-            @log ", $nᵖ, $(L∞(p)), $r₂\n"
-            r₂<tol && break
-        end
-        perBC!(p.x,p.perdir)
+    while nᵖ<itmx
+        Vcycle!(ml)
+        smooth!(p)
+        r₂ = L₂(p)
+        nᵖ+=1
+        @log ", $nᵖ, $(L∞(p)), $r₂\n"
+        r₂<tol && break
     end
+    perBC!(p.x,p.perdir)
     push!(ml.n,nᵖ);
 end
