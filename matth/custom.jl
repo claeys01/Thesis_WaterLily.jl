b4fa0786f1ab1c422040dae8f8b7daa837360618
claeys01 <matthias.claeys2002@gmail.com>
2025-10-01 10:27:44 +0200

    added new functions to seperate file

---
+++ matth/custom.jl
@@ -0,0 +1,18 @@
+using WaterLily
+import WaterLily: ∂, @loop
+
+function grad(field::AbstractArray)
+    T = eltype(field)
+    sz = size(field)
+    N = ndims(field)
+    grad = zeros(T, sz..., N)  # e.g., zeros(Float32, Nx, Ny, 2)
+    for n in 1:N
+        @loop grad[Tuple(I)..., n] = ∂(n, I, field) over I ∈ inside(field)
+    end
+    return grad
+end
+
+function RHS(flow::Flow{N};λ=WaterLily.quick,udf=nothing,kwargs...) where N
+    RHS = WaterLily.conv_diff!(flow.f,flow.u⁰,flow.σ,λ;ν=flow.ν,perdir=flow.perdir) - grad(flow.p)
+    return RHS
+end
