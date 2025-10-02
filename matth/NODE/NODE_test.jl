1eb85baf042714d9d8a3a49abbb28fd2cb07602c
claeys01 <matthias.claeys2002@gmail.com>
2025-10-02 11:24:38 +0200

    first attempt at running NODE with flow data

--- matth/NODE/NODE_test.jl
+++ matth/NODE/NODE_test.jl
@@ -8,7 +8,7 @@ u0 = Float32[2.0; 0.0]
 datasize = 30
 tspan = (0.0f0, 1.5f0)
 tsteps = range(tspan[1], tspan[2]; length = datasize)
-
+println(tsteps)
 function trueODEfunc(du, u, p, t)
     true_A = [-0.1 2.0; -2.0 -0.1]
     du .= ((u .^ 3)'true_A)'
@@ -16,6 +16,7 @@ end
 
 prob_trueode = ODEProblem(trueODEfunc, u0, tspan)
 ode_data = @timeit to "solve true ODE" Array(solve(prob_trueode, Tsit5(); saveat = tsteps))
+
 
 dudt2 = Chain(x -> x .^ 3, Dense(2, 50, tanh), Dense(50, 2))
 p, st = @timeit to "Lux setup" Lux.setup(rng, dudt2)
